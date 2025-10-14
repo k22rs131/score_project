7786b7ff09963cce932defa21bd32b171f3b1efb
k22rs131 <k22rs131@st.kyusan-u.ac.jp>
2025-10-14 14:37:42 +0900

    管理者画面でもファイルを確認

--- score/admin.py
+++ score/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
-from .models import Score
+from django.utils.html import format_html
+from .models import Score, ScoreFile
 
-admin.site.register(Score)
-# Register your models here.
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'comp', 'arr', 'category', 'created_at')
+    search_fields = ('title', 'comp', 'arr')
+    list_filter = ('category',)
+    ordering = ('title',)
+
+
+@admin.register(ScoreFile)
+class ScoreFileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'score', 'file_preview', 'uploaded_at')
+    list_filter = ('uploaded_at',)
+    readonly_fields = ('file_preview',)
+
+    def file_preview(self, obj):
+        """Cloudinaryファイルをプレビューまたはリンク表示"""
+        if not obj.file:
+            return "(ファイルなし)"
+
+        url = obj.file.url
+
+        if url.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp')):
+            return format_html('<img src="{}" width="150" style="border:1px solid #ccc;"/>', url)
+        elif url.lower().endswith('.pdf'):
+            return format_html('<a href="{}" target="_blank">PDFを開く</a>', url)
+        else:
+            return format_html('<a href="{}" target="_blank">ファイルを開く</a>', url)
+
+    file_preview.short_description = "ファイルプレビュー"
